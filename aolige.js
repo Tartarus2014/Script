5e2a10ed3f6bfdb52e01b828cff0fdc4a01387c4
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-09-10 20:40:19 +0800

    Update aolige.js

--- aolige.js
+++ aolige.js
@@ -1,6 +1,8 @@
-//  http-response ^https://1008610010\.yohui\.vip/index\.php/Api/LiveApi/getMovietime,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/aolige.js
+//  http-response ^https://1008610010\.yohui\.vip/index\.php/Api/LiveApi/getMovietime requires-body=1,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/aolige.js,requires-body=true,timeout=10,tag=aoligei
 
 //   mitm=1008610010.yohui.vip
+
+//  邀请码175957
 
 // 下载链接   https://ok6.app
 
