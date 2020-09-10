89f6212475500dc3f003f01d772637222fe9c6c4
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-09-10 19:59:07 +0800

    Update aolige.js

--- aolige.js
+++ aolige.js
@@ -1,8 +1,8 @@
-#  http-response ^https://1008610010\.yohui\.vip/index\.php/Api/LiveApi/getMovietime,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/aolige.js
+//  http-response ^https://1008610010\.yohui\.vip/index\.php/Api/LiveApi/getMovietime,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/aolige.js
 
-#   mitm=1008610010.yohui.vip
+//   mitm=1008610010.yohui.vip
 
-#   下载链接   https://ok6.app
+// 下载链接   https://ok6.app
 
 
 console.log($response.body);
