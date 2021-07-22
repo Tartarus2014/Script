ece652ccee2e2a2c1e36667100acb001aceb13fd
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2021-07-22 17:58:29 +0800

    Update CaiYun.js

--- CaiYun.js
+++ CaiYun.js
@@ -3,11 +3,11 @@
  * hostname = biz.caiyunapp.com	
  * 	
  * Surge：
- * [Script]	
+[Script]	
 彩云天气SVIP = type=http-response,requires-body=1,max-size=0,pattern=https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user),script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/CaiYun.js
  * 
  * Loon：
- * [Script]	
+[Script]	
  http-response https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user),script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/CaiYun.js,requires-body=1, tag=彩云天气SVIP	
  */	
  
