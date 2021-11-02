be300e628bc66b8b15b60e00f9b5cd25969ac7fb
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2021-11-02 09:28:49 +0800

    Update jdjf_price.js

--- jdjf_price.js
+++ jdjf_price.js
@@ -1,10 +1,15 @@
 /*
-Loon/Surge：
+Loon/Surge
 [Script]
 http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) requires-body=1,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/jdjf_price.js, tag=京东比价
 
 [MITM]
 hostname = api.m.jd.com
+
+QX:
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/jdjf_price.js
+
+Boxjs订阅：https://raw.githubusercontent.com/githubdulong/Script/master/boxjs.json
 */
  
 const path1 = "serverConfig";
