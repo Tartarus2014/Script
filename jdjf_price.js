4e553c45a6bab282087e42462b1af7645280a67e
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2021-11-02 18:41:05 +0800

    Update jdjf_price.js

--- jdjf_price.js
+++ jdjf_price.js
@@ -144,7 +144,7 @@ if (url.indexOf(path2) != -1 || url.indexOf(path4) != -1) {
                 msg += detail[0];
             }
             let oprnUrl = detail[1].convertURL ? detail[1].convertURL : "";
-            $tool.notify("京东价格/返利", "", msg, oprnUrl);
+            $tool.notify("京东联盟", "", msg, oprnUrl);
         })
         .finally(() => {
             $done({ body });
@@ -154,7 +154,7 @@ if (url.indexOf(path2) != -1 || url.indexOf(path4) != -1) {
 function lowerMsgs(data) {
     const lower = data.lowerPriceyh;
     const lowerDate = dateFormat(data.lowerDateyh);
-        const lowerMsg = "💴 历史最低 ➩ " + String(lower) + "¥║" + `🏷 ${lowerDate}`;
+        const lowerMsg = "〽️ 历史最低 ➩ " + String(lower) + " ║" + `🗓 ${lowerDate}`;
     return lowerMsg;
 }
 
@@ -367,7 +367,7 @@ function convert(url, isOriginJXURL) {
                         let r = {};
                         let scheme = autoChoose ? autoScheme : chooseScheme;
                         if (data.data.promotionUrl) {
-                            r.msg = `💴 预估返利 ➩ ${data.data.wlCommission}¥║〽️ 返利比例 ➩ ${data.data.wlCommissionShare}%`;
+                            r.msg = `💴 预计返利 ➩ ${data.data.wlCommission}`+ " ║" + `🏷 返利率 ➩ ${data.data.wlCommissionShare}%`;
                             r.convertURL =
                                 scheme == "browser"
                                     ? chooseBrowser + data.data.promotionUrl
